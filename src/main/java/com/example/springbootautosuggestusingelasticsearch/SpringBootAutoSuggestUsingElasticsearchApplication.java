8bfcae9e6259a8f67086f0b03b4d0e848a1f07b0
Ravi Kumar Gupta <111415169+EnggAdda@users.noreply.github.com>
2023-07-31 14:12:46 +0530

    added git comments

--- src/main/java/com/example/springbootautosuggestusingelasticsearch/SpringBootAutoSuggestUsingElasticsearchApplication.java
+++ src/main/java/com/example/springbootautosuggestusingelasticsearch/SpringBootAutoSuggestUsingElasticsearchApplication.java
@@ -9,5 +9,6 @@ public class SpringBootAutoSuggestUsingElasticsearchApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringBootAutoSuggestUsingElasticsearchApplication.class, args);
     }
+    //git comments
 
 }
